527ad82e42bf35b08f9fa7eaf9661769825ab223
ahmedheroo <69500751+ahmedheroo@users.noreply.github.com>
2021-08-27 06:58:45 +0200

    7

--- CourseCenterMS/CourseCenterMS/Form1.cs
+++ CourseCenterMS/CourseCenterMS/Form1.cs
@@ -13,9 +13,12 @@ namespace CourseCenterMS
 {
     public partial class Dashbord : Form
     {
+        int ph; bool hided;
         public Dashbord()
         {
             InitializeComponent();
+            ph = pnlStudent.Height;
+            hided = false;
         }
 
         private void btnExit_Click(object sender, EventArgs e)
@@ -30,11 +33,11 @@ namespace CourseCenterMS
 
         private void btnAllStudents_Click(object sender, EventArgs e)
         {
-            CourseCenterEntities context = new CourseCenterEntities();
-            frmAllStudents f = new frmAllStudents();
-            ContainerPnl.Controls.Clear();
-            f.grdAllStudents.DataSource = context.Students.Select(x => new { x.Name, x.GroupID, x.Classroom, x.Phone,x.ID }).ToList(); ;
-            ContainerPnl.Controls.Add(f.pnlAllStudents);
+            //CourseCenterEntities context = new CourseCenterEntities();
+            //frmAllStudents f = new frmAllStudents();
+            //ContainerPnl.Controls.Clear();
+            //f.grdAllStudents.DataSource = context.Students.Select(x => new { x.Name, x.GroupID, x.Classroom, x.Phone,x.ID }).ToList(); ;
+            //ContainerPnl.Controls.Add(f.pnlAllStudents);
         }
 
         private void btnMenuAddStudent_Click(object sender, EventArgs e)
@@ -42,6 +45,60 @@ namespace CourseCenterMS
             frmNewStudent f = new frmNewStudent();
             ContainerPnl.Controls.Clear();
             ContainerPnl.Controls.Add(f.pnlNewStudent);
+        }
+
+        private void btnStudent_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+           // Helper.ShowSubMenu(pnlStudent);
+ 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Show(pnlGroups);
+            //Helper.ShowSubMenu(pnlGroups);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Helper.ShowSubMenu(pnlPresence);
+
+        }
+        public void Show(Panel subMenu)
+        {
+            if (hided)
+            {
+                subMenu.Height = subMenu.Height + 5;
+                if (subMenu.Height >= ph)
+                {
+                    timer1.Stop();
+                    hided = false;
+                  
+                }
+            }
+            else
+            {
+                subMenu.Height = subMenu.Height - 5;
+                if (subMenu.Height <= 0)
+                {
+                    timer1.Stop();
+                    hided = true;
+            
+                }
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Show(pnlStudent);
+        }
+
+        private void Dashbord_Load(object sender, EventArgs e)
+        {
+            pnlStudent.Height = 0;
+            pnlGroups.Height = 0;
+            pnlPresence.Height = 0;
         }
     }
 }
